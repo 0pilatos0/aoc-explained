014fe33360fd7747cb2c0bdf66a2beaa570abb49
0pilatos0 <Paulvanderlei@hotmail.nl>
2023-11-22 23:27:41 +0100

    Add react-code-blocks package and update page component

--- src/app/2023/page.tsx
+++ src/app/2023/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Year2023 from "@/components/year2023";
 import React from "react";
+import { CodeBlock, dracula } from "react-code-blocks";
 
 function page() {
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <h3 className="text-1xl font-extrabold tracking-tight text-white sm:text-[2rem]">
         2023 Solutions and Explanations
       </h3>
@@ -13,10 +16,44 @@ function page() {
         it. but I can&apos;t promise anything.
       </p>
 
-      <div className="py-4">
-        <Year2023 />
-      </div>
-    </>
+      <hr className="w-full border-[#ffffff] border-opacity-10" />
+      <p>
+        For the challange of this year ill be using a template i made to speed
+        up the initial setup each day, the setup is using the bun runtime for
+        easy support for typescript. In the template the initial setup of
+        loading the input file and parsing it is already done, so i can focus on
+        the challange itself.
+      </p>
+      <p>
+        The template is available on my github repo{" "}
+        <a
+          href="https://github.com/0pilatos0/adventofcode/tree/master/template"
+          target="_blank"
+          className="text-[#ff0000] hover:text-[#ff0000] hover:underline"
+        >
+          here
+        </a>
+        . Feel free to use it if you want.
+      </p>
+
+      <p>
+        At the top of the setup file is a constant to switch between the test
+        input and the real input. The test input is a small input that is used
+        to test the solution, and the real input is the actual input for the
+        challange. The test input is usually a small subset of the real input,
+        and is used to test the solution before running it on the real input.
+      </p>
+
+      <CodeBlock
+        text="const USETESTINPUT = false;"
+        language="typescript"
+        theme={dracula}
+      />
+
+      <hr className="w-full border-[#ffffff] border-opacity-10" />
+
+      <Year2023 />
+    </div>
   );
 }
 
